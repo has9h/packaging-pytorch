a3427ec92f9e854b56d643bb98ddb4f1bf0f06b5
Hasnain Hossain <hasnainhossain@yahoo.com>
2022-07-10 02:42:08 +0600

    tentative resolve: opencv bundling

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-import numpy as np
+# import numpy as np
+import os
+import sys
+# import torch
+# print(torch.cuda.is_available())
+try:
+    import cv2
+except Exception as e:
+    print('Error bundling cv2')
+    print(e)
+print(cv2.__file__)
+print(sys.builtin_module_names)
+print(globals())
+
 
 input('Here to wait.')
