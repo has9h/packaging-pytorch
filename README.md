4d76a1636f393958171b50ff1bfee38de6765154
Hasnain Hossain <hasnainhossain@yahoo.com>
2022-07-12 23:41:14 +0600

    TODO: resolve bedId argparse

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
-# Packaging PyTorch with [PyInstaller](https://github.com/pyinstaller/pyinstaller-hooks-contrib)
+# Packaging ~PyTorch~ *opencv* with [PyInstaller](https://github.com/pyinstaller/pyinstaller-hooks-contrib)
+
+**Update: It seems the culprit when bundling with `easyocr` is not PyTorch, but `opencv`**
+
+## Changelog
+
+- [*See here*](https://stackoverflow.com/questions/67494128/pyinstaller-modulenotfounderror-no-module-named-cv2)
+    - `pyinstaller` downgraded to 4.5.1
+    - `pyinstaller-hooks-contrib` downgraded to 2021.3, from 2022
 
 PyInstaller hooks are required to allow PyTorch to be bundled. The hooks package should automatically be installed when installing PyInstaller.
 
 The documentation provides more details into Hooks, and how to bundle(Page 69, [Understanding PyInstaller Hooks](https://readthedocs.org/projects/pyinstaller/downloads/pdf/latest/)).
 
 `cx_Freeze` [may also be required](https://stackoverflow.com/questions/41570359/how-can-i-convert-a-py-to-exe-for-python) to bundle a Python script with PyTorch.
+
+## Bundling the Package
+
+Run the following, before navigating and executing `./dist/main/main.exe`
+
+```
+pyinstaller --collect-all easyocr main.py
+```
--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# import numpy as np
+# -*- coding: utf-8 -*-
+
+from os import path
+from datetime import datetime
+from collections import ChainMap
+from logging.handlers import RotatingFileHandler
+
 import os
 import sys
-# import torch
-# print(torch.cuda.is_available())
+import json
+import time
+import logging
+
 try:
     import cv2
+    print(cv2.__file__)
+    print('Successfully loaded cv2')
+    try:
+        import easyocr
+        print('Successfully loaded easyocr')
+    except Exception as ocr_err:
+        print('easyocr failed to bundle')
 except Exception as e:
     print('Error bundling cv2')
     print(e)
-print(cv2.__file__)
-print(sys.builtin_module_names)
-print(globals())
+
+try:
+    reader = easyocr.Reader(['en'], gpu=False)   
+    # Custom logger
+    logger = logging.getLogger(__name__)
+
+    # Create handler for logs
+    # Remember to manually create the directory
+    handler = RotatingFileHandler(path.join(path.abspath('logs'), logger.name), mode='a', maxBytes=10000, backupCount=10)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    logger.debug('OCR script running')
+except Exception as e:
+    print(e)
+
 
 
 input('Here to wait.')
